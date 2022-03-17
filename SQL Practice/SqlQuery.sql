2cff4cd1bcc2222e90d53db2cac2116a371958a3
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-03-17 23:09:53 +0530

    added Database for practice

--- SQL Practice/SqlQuery.sql
+++ SQL Practice/SqlQuery.sql
@@ -12,3 +12,13 @@ INSERT INTO player(name, age,score) VALUES("Rahul", 50, 200);
 INSERT INTO player(name, age,score) VALUES("Jadwja", 35, 300);
 
 SELECT * FROM player;
+
+SELECT name, age, score FROM player;
+
+SELECT name FROM player;
+
+INSERT INTO player
+(name, age,score) 
+VALUES
+("Jadwja", 35, 300),
+("Dhoni", 35, 300);

9b8f35ce01af2d0d1e22ed626107a18f9cf60898
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-04-01 18:12:00 +0530

    Added er model to db transition related files and scripts

--- SQL Practice/ERModel_to_DB_Mapping.sql
+++ SQL Practice/ERModel_to_DB_Mapping.sql
@@ -6,6 +6,7 @@
 -- Address
 */
 
+-- CREATE TABLE
 
 	CREATE TABLE 
 		customer(
@@ -58,8 +59,70 @@
 		);
 
 	PRAGMA TABLE_INFO(cart);
+	
+-- cart and product junction TABLE
+
+		CREATE TABLE 
+			cart_product(
+				id INTEGER NOT NULL PRIMARY KEY,
+				cart_id INTEGER,
+				product_id INTEGER,
+				quantity INTEGER,
+				FOREIGN KEY(cart_id )REFERENCES cart(id) ON DELETE CASCADE,
+				FOREIGN KEY(product_id) REFERENCES product(id) ON DELETE CASCADE
+			);
+
+			
+-------------------------------------------------------------------------------
+-- INSERT DATA
+
+	INSERT INTO
+		customer(id, name, age)
+	VALUES
+		(1, "John", 29),
+		(2, "Emma", 24);
+		
+	SELECT *FROM customer;
+	
+	INSERT INTO
+		address(id, pin_code, door_no, city, customer_id)
+	VALUES
+		(1001, 517130, "6-1", "Hyderabad", 1),
+		(1002, 615670, "6-13", "Bengaluru", 1);
+		
+	SELECT *FROM address;
+	
+	INSERT INTO
+		cart(id, total_price, customer_id)
+	VALUES
+		(1, 12000, 1),
+		(2, 500, 2);
+		
+	UPDATE  cart SET total_price = 1500 WHERE id = 2;	
+		
+	SELECT *FROM cart;
+	
+	
+	INSERT INTO 
+		product(id, name, price, brand, category)
+	VALUES
+		(1, "Jeans", 1500, "Pepe", "Clothing"),
+		(2, "T-shirt", 500, "Puma", "Clothing"),
+		(3, "Mobile", 10000, "Redmi", "Gadgets"),
+		(4, "Watch", 2000, "Titan", "Gadgets");
+			
+	SELECT *FROM product;
+	
+	INSERT INTO
+		cart_product(id, cart_id, product_id, quantity)
+	VALUES
+		(1, 1, 1, 1),
+		(2, 1, 2, 1),
+		(3, 1, 3, 1),
+		(4, 2, 1, 1);
+		
+	SELECT *FROM cart_product;
 
 		
-	
 
 

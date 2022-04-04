7dae96e81f1e1baa11b85127d3a6dc60fec56480
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-04-04 15:26:14 +0530

    Added eCommerce DB and its sql file

---
+++ SQL Practice/eCommerce.sql
@@ -0,0 +1,64 @@
+
+-- DB Schema
+
+	CREATE TABLE user(
+		id INTEGER NOT NULL PRIMARY KEY,
+		name TEXT,
+		age INTEGER,
+		gender TEXT,
+		phone_no INTEGER,
+		address TEXT,
+		pincode INTEGER
+	);
+	
+	PRAGMA TABLE_INFO(user);
+	
+	CREATE TABLE order_details(
+		order_id INTEGER NOT NULL PRIMARY KEY,
+		customer_id INTEGER,
+		order_date TEXT,
+		shipped_date TEXT,
+		shipped_id INTEGER,
+		total_amount INTEGER,
+		FOREIGN KEY (customer_id) REFERENCES user(id) ON DELETE CASCADE
+	);
+	
+	CREATE TABLE product(
+		product_id INTEGER NOT NULL PRIMARY KEY,
+		name TEXT,
+		price_per_unit INTEGER,
+		rating INTEGER,
+		category TEXT,
+		brand TEXT
+	);	
+		
+	CREATE TABLE order_product(
+		id INTEGER NOT NULL PRIMARY KEY,
+		order_id INTEGER,
+		product_id INTEGER,
+		no_of_units INTEGER,
+		FOREIGN KEY (order_id) REFERENCES order_details(order_id) ON DELETE CASCADE,
+		FOREIGN KEY (product_id) REFERENCES product(product_id) ON DELETE CASCADE
+	);
+		
+------------------------------------------------
+-- Data insertion done manually		
+
+-----------------------------------------------------
+-- QUERYING
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
